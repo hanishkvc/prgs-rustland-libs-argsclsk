5fdd0d0c2000b55fc8da19c1811a49fea7e7107f
hanishkvc <hanishkvc@gmail.com>
2022-08-21 22:19:14 +0530

    Track Var mutably shared with closure, through key points in flow

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 use simpcmdlinek;
 use loggerk::{log_init, log_d};
 
-fn test_cmdline(mut key3magic: u32) {
+fn test_cmdline(mut sharedvar: u32) {
     let mut sclm = simpcmdlinek::SimpCmdLineManager::new();
 
     let mut key1h = |iarg: usize, theargs: &Vec<String>| -> usize {
@@ -21,15 +21,20 @@ fn test_cmdline(mut key3magic: u32) {
         1
     };
     sclm.add_handler("--key2", &mut key2h);
+
+
+    log_d(&format!("DBUG:TestCmdLine:SharedVar:B4DefiningClosure:{}", sharedvar));
     let mut key3h = |iarg, theargs: &Vec<String>|-> usize {
-        let localmagic = key3magic;
-        key3magic += 3;
+        let localmagic = sharedvar;
+        sharedvar += 3;
         log_d(&format!("FoundArg:@{}:{}: Also key3magic is {}", iarg, theargs[iarg], localmagic));
         0
     };
     sclm.add_handler("--key3", &mut key3h);
+    //log_d(&format!("DBUG:TestCmdLine:SharedVar:B4ProcessArgs:{}", sharedvar));
     sclm.process_args();
-    drop(sclm);
+    log_d(&format!("DBUG:TestCmdLine:SharedVar:AfterProcessArgs:{}", sharedvar));
+    //drop(sclm); // By commenting out this drop, we allow compiler to auto drop it immidiately after process_args automatically
 }
 
 
